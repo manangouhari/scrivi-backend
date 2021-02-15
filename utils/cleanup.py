de7f9efb1b000949626b70e8f0944a5330b76cd7
Manan Gouhari <gouhari21@hotmail.com>
2021-02-15 16:28:23 +0530

    📚 Doumentation for utils/cleanup.py

--- utils/cleanup.py
+++ utils/cleanup.py
@@ -1,36 +1,50 @@
 import re
 
-# def cleanup(sent):
-#     filter_arr = []
-#     words = sent.words
-#     tokens = []
-#     for word in words:
-
-#         if not (word.is_stop or word.is_punct or word.is_space or word.text == '=' or word.lemma_ == '-PRON-'):
-#             tokens.append(re.sub(r'[\t\n\r=]', '', word.lemma_.lower()))
-
-#     return ' '.join(tokens)
-
-
-# def sanitize_for_model(sents):
-#     clean_func = np.vectorize(cleanup)
-#     return clean_func(sents)
-
-
-
-# def sanitize_original(sent):
-#     return re.sub(r'[\t\n\r]', '', sent)
-
-
 def contains_digit(word):
+  """
+    Checks whether a word contains a digit or not.
+      Parameters:
+        word (str): string that needs to be checked.
+      Returns:
+        boolean: True is word contains a digit, else False.
+  """
   for ch in word:
     if ch.isdigit(): return True
   return False
 
 def clean_word(word):
+  """
+    Cleans up a word. 
+    It does the following three thing -
+      1. Convert to lowercase.
+      2. Singularise the word.
+      3. Removes single and double quotes.
+
+      Parameters:
+        word (str): word that needs to be cleaned up.
+      Reurns:
+        word (str): cleaned up version of the word.
+  """
   return re.sub(r'\'\"' ,'', word.singularize().lower())
 
 def filter_words(words, stopwords):
+  """
+    Removes words that don't match the criteria. 
+    Then cleans up the words.
+    Returns a list that contains the cleaned up form of words that match the criteria.
+    Criteria to include a word -
+      1. Should not be a stopword. 
+        - Stopwords: https://en.wikipedia.org/wiki/Stop_word
+      2. Should not contain digits. 
+      3, Should not contain single quotes. Basically to get rid of contractions - shouldn't, wouldn't, etc.
+    
+      Parameters:
+        words (list[str]): list of words in the text.
+        stopwords (list[str]): list of stopwords. 
+
+      Returns:
+        wordlist (list[str]): list of cleaned up words that match the criteria.
+  """
   return list(map(clean_word ,filter(lambda x: x not in stopwords and not contains_digit(x) and '\'' not in x, words)))
 
 def clean_sentence(sentence):

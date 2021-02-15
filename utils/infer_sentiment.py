f8f10f60e0b9db567829b9dee9955dd0d1c86d85
Manan Gouhari <gouhari21@hotmail.com>
2021-02-15 16:31:10 +0530

    📚 doc for utils/infer_sentiment.py

--- utils/infer_sentiment.py
+++ utils/infer_sentiment.py
@@ -1,4 +1,11 @@
 def infer_sentiment(sentiment):
+  """
+    Returns a label based on sentiment polarity score.
+    Parameters:
+      sentiment (float): represents the sentiment polarity score for the text.
+    Returns
+      result ([sentiment, label]): a list of size 2. First item being the sentiment score, second being the label.
+  """
   label = ''
   if sentiment < -0.5:
     label = 'SUPER NEGATIVE'
